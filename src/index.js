2262be8587b5787dfe6c74354f570a246354639e
sougata <sougata19582001@gmail.com>
2023-12-16 19:16:48 +0530

    created the fundamental components of react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { React } from "react";
-import { ReactDOM } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
   return <h1>Hello React!</h1>;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('id'));
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
