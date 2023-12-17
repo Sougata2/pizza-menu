6a0bdb1b5f3ac39a81611ba18417b9972ef626eb
sougata <sougata19582001@gmail.com>
2023-12-17 21:58:03 +0530

    Updates

---
+++ precache-manifest.9781544068457d362634742f41cb200d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e7039b1dbf9b270f312b950679258b",
+    "url": "/pizza-menu/index.html"
+  },
+  {
+    "revision": "eb7f4af5f7ed2d6cb1c0",
+    "url": "/pizza-menu/static/css/main.94b588da.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/pizza-menu/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "eb7f4af5f7ed2d6cb1c0",
+    "url": "/pizza-menu/static/js/main.80787dc6.chunk.js"
+  },
+  {
+    "revision": "a8638b09d05fd16587f5",
+    "url": "/pizza-menu/static/js/runtime~main.583274af.js"
+  }
+]);
